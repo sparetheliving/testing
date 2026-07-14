5967f1634f1d6686265e1f8f154318090c0b5703
threats <102300567+sparetheliving@users.noreply.github.com>
2026-07-14 06:07:32 +0000

    Refactor hitboxexpander.lua by removing unused variables

--- hitboxexpander.lua
+++ hitboxexpander.lua
@@ -1,9 +1,6 @@
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 
-local CharacterAddedEvents = {}
-local ChildAdded = {}
-
 local function ChangeHitbox(Target : Model) : Part
 	local HumanoidRootPart = Target:WaitForChild("HumanoidRootPart", 1)
 	if not HumanoidRootPart then return end
