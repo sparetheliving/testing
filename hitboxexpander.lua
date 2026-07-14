890ed2fe7eeed2443b31808270d8771e64573a56
threats <102300567+sparetheliving@users.noreply.github.com>
2026-07-14 06:05:35 +0000

    Refactor hitbox setup and player connection logic

--- hitboxexpander.lua
+++ hitboxexpander.lua
@@ -18,41 +18,11 @@ local function ChangeHitbox(Target : Model) : Part
 	return RootAttachment
 end
 
-local function Setup(nigga : Player)
-	local RootAttachment = ChangeHitbox(nigga.Character)
-	if not RootAttachment then return end
-	if ChildAdded[nigga.Name] then
-		ChildAdded[nigga.Name]:Disconnect()
+for _, nigga in Players:GetPlayers() do
+	if nigga == Player then continue end
+	if nigga.Character then
+		ChangeHitbox(nigga.Character)
 	end
-	ChildAdded[nigga.Name] = RootAttachment.ChildAdded:Connect(function(Part)
-		if Part.Name == "Hitbox" then
-			ChangeHitbox(Part)
-		end
-	end)
-	
-	if CharacterAddedEvents[nigga.Name] then
-		CharacterAddedEvents[nigga.Name]:Disconnect()
-	end
-	CharacterAddedEvents[nigga.Name] = nigga.CharacterAdded:Connect(function(Character)
-		ChangeHitbox(Character)
-	end)
 end
-
-Players.PlayerAdded:Connect(function(nigga)
-	nigga.CharacterAdded:Wait()
-	task.wait(.1)
-	Setup(nigga)
-end)
-
-Players.PlayerRemoving:Connect(function(nigga)
-	if CharacterAddedEvents[nigga.Name] then
-		CharacterAddedEvents[nigga.Name]:Disconnect()
-		CharacterAddedEvents[nigga.Name] = nil
-	end
-	if ChildAdded[nigga.Name] then
-		ChildAdded[nigga.Name]:Disconnect()
-		ChildAdded[nigga.Name] = nil
-	end
-end)
 
 warn("Hitbox expander activated")
